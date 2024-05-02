a8823d7e3f6084a76b67ee9364e42bbc497841f8
latteisacat <dragonldh0907@gmail.com>
2024-05-02 20:54:24 +0900

    fix: 누락된 어노테이션 추가

--- capstone_backend/src/main/java/com/example/capstone_backend/domain/account/controller/AccountController.java
+++ capstone_backend/src/main/java/com/example/capstone_backend/domain/account/controller/AccountController.java
@@ -2,11 +2,16 @@ package com.example.capstone_backend.domain.account.controller;
 
 import com.example.capstone_backend.domain.account.dto.request.JoinRequestDTO;
 import com.example.capstone_backend.domain.account.dto.request.LoginRequestDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("")
 public class AccountController {
     //login api
     @PostMapping("/login")
