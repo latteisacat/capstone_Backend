e0916894e5bd76840c208e7c2beca5ee36856029
latteisacat <dragonldh0907@gmail.com>
2024-05-18 20:51:59 +0900

    fix: 추천유저 범위 유동적으로 수정하는 메소드 추가

--- capstone_backend/src/main/java/com/example/capstone_backend/domain/home/service/HomeReadService.java
+++ capstone_backend/src/main/java/com/example/capstone_backend/domain/home/service/HomeReadService.java
@@ -43,10 +43,17 @@ public class HomeReadService {
 
         List<UserHomeResponseDTO.AverageRecord> averageRecords = getAverageRecord(myExerciseList, sex);
 
-        List<UserHomeResponseDTO.RecommendedUser> recommendedUsers =
-                userInfoRepository.getRecommendedUsers(user.getBodyScore(), sex, user.getId())
-                        .stream().map(UserHomeResponseDTO.RecommendedUser::of).toList();
-        recommendedUsers = recommendedUsers.stream().filter(recommendedUser -> !competitorIds.contains(recommendedUser.userId())).toList();
+        Double range = 0.15;
+        Integer count = 0;
+        List<UserHomeResponseDTO.RecommendedUser> recommendedUsers;
+        do{
+            count += 1;
+            recommendedUsers = userInfoRepository.getRecommendedUsers(user.getBodyScore(), sex, user.getId(), range)
+                    .stream().map(UserHomeResponseDTO.RecommendedUser::of).toList();
+            recommendedUsers = recommendedUsers.stream().filter(recommendedUser -> !competitorIds.contains(recommendedUser.userId())).toList();
+            System.out.println("사이즈 : " + recommendedUsers.size());
+            range += 0.05;
+        }while(recommendedUsers.size() == 0 ||  count < 5);
 
         List<UserCompetitorDTO> userCompetitorDTOList = getUserCompetitorDTOList(user, myCompetitors);
 
--- capstone_backend/src/main/java/com/example/capstone_backend/domain/user/UserInfoRepository.java
+++ capstone_backend/src/main/java/com/example/capstone_backend/domain/user/UserInfoRepository.java
@@ -20,11 +20,13 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     )
     Long getBetterBodyScoreUserCount( @Param("bodyScore") Double bodyScore, @Param("sex") String sex);
 
-
-    //TODO: bodyScore로 검색하는 범위 적절히 수정 필요
     @Query("select u from UserInfo u" +
-            " where  :bodyScore*0.7 < u.bodyScore and u.bodyScore < :bodyScore*1.3 and u.sex = :sex"
+            " where  :bodyScore*(1.0 - :range) < u.bodyScore and u.bodyScore < :bodyScore*(1.0 + :range) and u.sex = :sex"
             + " and u.isDummy = false " + "and u.id != :userId "
     )
-    List<UserInfo> getRecommendedUsers( @Param("bodyScore") Double bodyScore, @Param("sex") String sex, @Param("userId") Long userId);
+    List<UserInfo> getRecommendedUsers(
+            @Param("bodyScore") Double bodyScore,
+            @Param("sex") String sex,
+            @Param("userId") Long userId,
+            @Param("range")Double range);
 }
