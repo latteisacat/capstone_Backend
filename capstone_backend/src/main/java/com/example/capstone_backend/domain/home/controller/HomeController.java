001e99e074d4918564a85adda7eba21e6c6ee15a
latteisacat <dragonldh0907@gmail.com>
2024-06-04 19:47:12 +0900

    fix: 왜 경쟁자 등록 메소드가 없는데 눈치를 못챈걸까

--- capstone_backend/src/main/java/com/example/capstone_backend/domain/home/controller/HomeController.java
+++ capstone_backend/src/main/java/com/example/capstone_backend/domain/home/controller/HomeController.java
@@ -2,17 +2,15 @@ package com.example.capstone_backend.domain.home.controller;
 
 
 import com.example.capstone_backend.common.jwt.CustomUserDetails;
+import com.example.capstone_backend.common.util.Tools;
 import com.example.capstone_backend.domain.home.service.HomeReadService;
-import com.example.capstone_backend.domain.user.dto.response.UserCompetitorDTO;
-import com.example.capstone_backend.domain.user.dto.response.UserHomeResponseDTO;
+import com.example.capstone_backend.domain.home.service.HomeWriteService;
+import com.example.capstone_backend.domain.user.dto.request.UserCompetitorAddDTO;
 import com.example.capstone_backend.common.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,6 +18,7 @@ import java.util.List;
 public class HomeController {
 
     final private HomeReadService homeReadService;
+    final private HomeWriteService homeWriteService;
 
     @GetMapping("/{userId}")
     public ResponseEntity<?> userHome(
@@ -28,159 +27,13 @@ public class HomeController {
         return ResponseEntity.ok(Response.success(homeReadService.getUserHome(userId)));
     }
 
-    private UserHomeResponseDTO dummy(){
-        UserHomeResponseDTO userHomeResponseDTO;
-
-        List<UserCompetitorDTO> competitors = new ArrayList<>();
-        List<List<UserCompetitorDTO.CompareDetail>> compareDetails = new ArrayList<>();
-        List<UserHomeResponseDTO.UserRecord> userRecords = new ArrayList<>();
-        List<UserHomeResponseDTO.RecommendedUser> recommendedUsers = new ArrayList<>();
-        List<UserHomeResponseDTO.AverageRecord> averageRecords = new ArrayList<>();
-        for(int i = 0; i < 6; i++){
-            compareDetails.add(new ArrayList<UserCompetitorDTO.CompareDetail>());
-        }
-
-        compareDetails.get(0).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("키")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-        compareDetails.get(1).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("몸무게")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-        compareDetails.get(2).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("근육량")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-        compareDetails.get(3).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("체지방량")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-        compareDetails.get(4).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("BMI")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-        compareDetails.get(5).add(UserCompetitorDTO.CompareDetail.builder()
-                .name("체지방률")
-                .me(169.0)
-                .competitor(174.0)
-                .build());
-
-        competitors.add(UserCompetitorDTO.builder()
-                .userId(1L)
-                .userProfile("safsaf")
-                .userName("asfasdfsd")
-                .userCompare(compareDetails)
-                .build());
-        competitors.add(UserCompetitorDTO.builder()
-                .userId(2L)
-                .userProfile("safsaf")
-                .userName("asfasdfsd")
-                .userCompare(compareDetails)
-                .build());
-        competitors.add(UserCompetitorDTO.builder()
-                .userId(3L)
-                .userProfile("safsaf")
-                .userName("asfasdfsd")
-                .userCompare(compareDetails)
-                .build());
-
-        userRecords.add(UserHomeResponseDTO.UserRecord.builder()
-                .sportName("데드리프트")
-                .record("160kg")
-                .percentage("30%")
-                .build());
-        userRecords.add(UserHomeResponseDTO.UserRecord.builder()
-                .sportName("벤치프레스")
-                .record("100kg")
-                .percentage("30%")
-                .build());
-        userRecords.add(UserHomeResponseDTO.UserRecord.builder()
-                .sportName("스쿼트")
-                .record("130kg")
-                .percentage("30%")
-                .build());
-
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(6l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(7l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(8l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(9l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(10l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-        recommendedUsers.add(UserHomeResponseDTO.RecommendedUser.builder()
-                .userId(11l)
-                .name("sfgsfdg")
-                .profile("345435afads")
-                .height(176.0)
-                .weight(69.0)
-                .build());
-
-        averageRecords.add(UserHomeResponseDTO.AverageRecord.builder()
-                .name("데드리프트")
-                .me("160kg")
-                .average("130kg")
-                .build());
-        averageRecords.add(UserHomeResponseDTO.AverageRecord.builder()
-                .name("벤치프레스")
-                .me("100kg")
-                .average("90kg")
-                .build());
-        averageRecords.add(UserHomeResponseDTO.AverageRecord.builder()
-                .name("스쿼트")
-                .me("130kg")
-                .average("110kg")
-                .build());
-
-        userHomeResponseDTO = UserHomeResponseDTO.builder()
-                .userId(234l)
-                .name("23423423")
-                .profile("oaidfoijasf")
-                .height(169.0)
-                .weight(65.4)
-                .muscleMass(32.5)
-                .bodyFat(9.5)
-                .percentageFat(14.5)
-                .userPercentage("20%")
-                .sex("남")
-                .competitors(competitors)
-                .userRecords(userRecords)
-                .graph(averageRecords)
-                .recommendedUsers(recommendedUsers)
-                .build();
-        return userHomeResponseDTO;
+    @PostMapping("/{userId}/addCompetitor")
+    public ResponseEntity<?> addCompetitor(
+            @PathVariable("userId") final UserCompetitorAddDTO userCompetitorAddDTO,
+            @AuthenticationPrincipal final CustomUserDetails userDetails
+    ){
+        Tools.invalidUserCheck(userDetails, userCompetitorAddDTO.userId());
+        homeWriteService.addCompetitor(userCompetitorAddDTO, userDetails.getUserInfo());
+        return ResponseEntity.ok(Response.success(null));
     }
 }
