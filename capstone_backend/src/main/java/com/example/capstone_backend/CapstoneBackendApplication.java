dbae3f59e39c80bc4152091d2a9635ab9d9baae1
latteisacat <dragonldh0907@gmail.com>
2024-06-20 15:49:13 +0900

    fix: aws 오류 수정

--- capstone_backend/src/main/java/com/example/capstone_backend/CapstoneBackendApplication.java
+++ capstone_backend/src/main/java/com/example/capstone_backend/CapstoneBackendApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 public class CapstoneBackendApplication {
 
 	public static void main(String[] args) {
